c7b01bc58ed43cb38c5f8398602654ee4b7c1dd1
Natalie Tejada <despain.natalie@gmail.com>
2026-03-05 10:35:42 -0700

    edits

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, defaultAllowedOrigins } from 'vite'
 import shopify from 'vite-plugin-shopify'
 import cleanup from '@by-association-only/vite-plugin-shopify-clean'
 
 export default defineConfig({
   plugins: [
     cleanup(),
-    shopify()
+    shopify({ snippetFile: 'vite-tag.liquid', tunnel: true })
   ],
+  server: {
+    cors: {
+      origin: [
+        defaultAllowedOrigins,
+        /.myshopify.com$/
+      ],
+    },
+  },
   build: {
     emptyOutDir: false
   }
